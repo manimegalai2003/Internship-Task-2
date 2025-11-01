c11d040e6d05f4b2f53a77d8075baf5286587136
manimegalai2003 <megalaim1015@gmail.com>
2025-11-01 11:25:07 +0530

    first commit

--- StringReverse.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str){
-    return str.split('').reverse().join('');
-}
-
-const str1 = "Hello";
-const rev = reverseString(str1);
-console.log("Original String: ",str1);
-console.log("Reversed String:" ,rev);
